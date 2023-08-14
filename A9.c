c133bb9d7e5535e7d9487e4bdeff34f057297a5b
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-08-14 21:54:03 +0530

    Initial draft.

---
+++ A9.c
@@ -0,0 +1,57 @@
+#pragma region	// Header declarations.
+#include <math.h>
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+// Prompts the user for input till valid input is received
+// using a `printf("Please enter the `%s`: ", message)` call.
+double ask_user_to_enter(const char *message);
+
+// Stores the user's input into `storage_addr` using `scanf()`,
+// returning `TRUE` or `FALSE` to state if it was a part of valid input.
+char store_valid_user_input_into(double *storage_addr);
+
+void clear_stdin(void);
+#pragma endregion
+
+int main() {
+  puts("Welcome to the base-to-power calculator!");
+
+  double base = ask_user_to_enter("base to give a power to");
+  double power = ask_user_to_enter("power to give the base");
+
+  printf("Here's the result!: %lf`", pow(base, power));
+
+  return 0;
+}
+
+double ask_user_to_enter(const char *p_message) {
+  double user_input;
+  char input_is_valid = FALSE;
+
+  while (!input_is_valid) {
+    printf("Please enter the `%s`: ", p_message);
+    input_is_valid = store_valid_user_input_into(&user_input);
+  }
+
+  return user_input;
+}
+
+char store_valid_user_input_into(double *p_storage_addr) {
+  const int scan_result = scanf("%lf", p_storage_addr);
+  clear_stdin();
+
+  if (scan_result == 0) {
+    puts("That doesn't seem to be valid input.");
+    return FALSE;
+  }
+
+  return TRUE;
+}
+
+void clear_stdin(void) {
+  for (char c; !(c == EOF || c == '\n'); c = getchar())
+    ;
+}
