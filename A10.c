c0f525590327cc92828effba5d8903838bae891b
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-08-15 12:05:14 +0530

    It's complete!

--- A10.c
+++ A10.c
@@ -1,19 +1,11 @@
-#pragma region	// Header declarations.
 #include <stdio.h>
-
-typedef unsigned long long ull;
-#pragma endregion
 
 int main() {
 	printf("Please enter a string to count characters in: ");
 
-	ull string_length = -1;
+	unsigned long long string_length = -1;
 	for (char c; c != '\n'; c = getchar(), string_length++)
 		;
-
-	// Unnecessary check:
-	// if (string_length == -1)
-	// 	string_length = 0;
 
 	printf("The length of the string is: `%lld` characters.\n", string_length);
 	return 0;
