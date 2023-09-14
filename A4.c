a0e8144556d809efabb9780958b00f82fe04c42d
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-09-14 19:53:52 +0530

    Hopefully, this is the final version!

--- A4.c
+++ A4.c
@@ -1,16 +1,18 @@
 // Author: Brahvim Bhaktvatsal
 
-#include "IoUtilsByBrahvim.h"
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
 
+#define OPS_LIST "`+`, `-`, `*`, " /* "`%`, `^` " */ "and `/`"
 
 // TODO: Calculator history! By detecting inputs such as `^[[A` and `^[[B`.
 
-typedef unsigned long long ull;
-
 int main() {
     puts("Hello there, and welcome to the simple calculator program!");
-    puts("Expressions such as `1234 + 5678` can be used here.");
-    puts("However, only the operators `+`, `-`, `*`, `/` are available.");
+    puts("Expressions such as `1234 + 5678` will be evaluated.");
+    puts("Note that only the operators " OPS_LIST " are available.");
+    puts("Use `^C` to exit.");
     puts("Enter your expressions!:");
 
     // These are not well-named because I later have to write this stuff BY HAND!:
@@ -20,13 +22,12 @@ int main() {
     }  /* __attribute__((packed)) */
     uin = {}; // I'd like to call this `user_input_expr` instead.
 
-    while (true) {
+    while (true) { // We loop forever so user input is always active.
         for (char input_is_valid = false; !input_is_valid;) {
             input_is_valid = scanf("%lf %c %lf", &uin.n1, &uin.op, &uin.n2) == 3;
 
-            // I like not having spaces in code-only comments, but VSCode doesn't this with other languages...:
-            //if (!input_is_valid)
-            //input_is_valid = scanf("%lf%c%lf", &uin.n1, &uin.op, &uin.n2) == 3;
+            for (char c; !(c = getchar() == '\n' || c == EOF);)
+                ;
 
             if (!input_is_valid) {
                 puts("Invalid input, try again!:");
@@ -48,21 +49,21 @@ int main() {
             break;
 
             case '/':
-            if (uin.n1 == 0 || uin.n2 == 0)
-                uin.res = 0;
-            else {
-                puts("Invalid operation.");
+            if (uin.n2 == 0) {
+                puts("Invalid operation - cannot divide by `0`.");
                 continue;
-            }
+            } else
+                uin.res = uin.n1 / uin.n2;
             break;
 
             default:
-            puts("Invalid operation! Only `+`, `-`, `*` and `/` are allowed, sorry! Try again:");
+            puts("Invalid operation! Only " OPS_LIST " are allowed, sorry! Try again:");
         }
 
         {
             long long rounded = uin.res;
 
+            // If it can be printed without `0`s, print the output without `0`s!:
             if (uin.res == rounded) {
                 printf("= `%lld`.\n", rounded);
                 continue;
--- IoUtilsByBrahvim.c
+++
@@ -1,6 +0,0 @@
-#include "IoUtilsByBrahvim.h"
-
-void clear_stdin(void) {
-    for (char c; !((c = getchar()) == '\n' || c == EOF);)
-        ;
-}
