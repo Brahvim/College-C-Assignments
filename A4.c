81354f333801b6f4512eb223fa3e38dddbd2d329
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-09-14 15:19:08 +0530

    Compiler-specific stuff!

--- A4.c
+++ A4.c
@@ -1,5 +1,30 @@
 #include "IoUtilsByBrahvim.h"
 
+typedef unsigned long long ull;
+
+#define OP_ADD '+'
+#define OP_SUB '-'
+#define OP_DIV '/'
+#define OP_MULT '*'
+
+#ifdef _MSC_VER
+#pragma pack(push, 1)
+#endif
+
+struct binary_expr {
+    char op;
+    ull n1, n2;
+#ifdef __GNUC__
+} __attribute__((packed));
+#else
+};
+#endif
+
+#ifdef _MSC_VER
+#pragma pack(pop)
+#endif
+
+
 int main() {
 
 }
