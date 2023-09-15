7108e891e21d9272c56d5dbc81504d75e79eac5b
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-09-15 19:21:35 +0530

    Modified `A6` to use the scan API + round off.

--- A6.c
+++ A6.c
@@ -1,63 +1,28 @@
 #pragma region	// Header declarations.
-#include <stdio.h>
+#include "include/IoUtilsByBrahvim.h"
 
-#define FALSE 0
-#define TRUE 1
-
-// Ensures that the user enters some valid input by
-// prompting them recursively, returning what is obtained.
-//
-// The user is prompted using `printf("Please enter the %s: ", message)`.
-double ask_user_to_enter(const char *message);
-
-// Stores user input into the passed buffer,
-// returning `TRUE` or `FALSE` to show it's validity as an `int`.
-char store_and_validate_user_input(double *storage_addr);
-
-// Clears `stdin`. Call after each call to `scanf()`!
-void clear_stdin(void);
-
+DECLARE_GENERIC_INPUT_FUNCTIONS(double);
 #pragma endregion
+
+DEFINE_GENERIC_INPUT_FUNCTIONS(double, "%lf");
 
 int main() {
 	puts("Welcome to the number comparison program!");
 
-	const double first_number = ask_user_to_enter("first number");
-	const double second_number = ask_user_to_enter("second number");
-	const double third_number = ask_user_to_enter("third number");
-
-	printf("The largest of the three is: `%lf`.\n",
-		first_number > second_number 
+	const double first_number = ensure_user_inputs_double("first number"),
+		second_number = ensure_user_inputs_double("second number"),
+		third_number = ensure_user_inputs_double("third number"),
+		largest = first_number > second_number
 		? (first_number > third_number ? first_number : third_number)
-		: (second_number > third_number ? second_number : third_number));
+		: (second_number > third_number ? second_number : third_number);
 
-	return 0;
-}
-
-double ask_user_to_enter(const char *p_message) {
-	double user_input;
-
-	for (char input_is_valid = FALSE; !input_is_valid;) {
-		printf("Please enter the %s: ", p_message);
-		input_is_valid = store_and_validate_user_input(&user_input);
+	{
+		unsigned long long rounded = (unsigned long long)largest;
+		if (largest == rounded) {
+			printf("The largest of the three is: `%llu`.\n", rounded);
+			return 0;
+		}
 	}
 
-	return user_input;
-}
-
-char store_and_validate_user_input(double *p_storage_addr) {
-	const int scan_result = scanf("%lf", p_storage_addr);
-	clear_stdin();
-
-	if (scan_result == 0) {
-		fputs("That's not valid input...\n\n", stderr);
-		return FALSE;
-	}
-
-	return TRUE;
-}
-
-void clear_stdin(void) {
-	for (char c; !(c == EOF || c == '\n'); c = getchar())
-		;
+	printf("The largest of the three is: `%lf`.\n", largest);
 }
