2bdb066e85aa2a4593aa45b9887d441af71d5443
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-09-17 21:29:16 +0530

    I might be... going somewhere.

--- A8.c
+++ A8.c
@@ -2,13 +2,30 @@
 
 #pragma region // Header declarations.
 #include "include/IoUtilsByBrahvim.h"
+#include <math.h>
+#include <string.h>
 
+// Structures:
+struct complete_result;
 struct student;
-struct result;
-struct marks;
 #pragma endregion
 
-#pragma region // Struct defintions.
+#pragma region // Structure definitions.
+struct student {
+    char *name;
+    unsigned long long regno;
+} /* __attribute__((packed)) */;
+
+struct complete_result {
+    float total;
+    float language, math, science;
+}/* __attribute__((packed)) */;
+#pragma endregion
+
+#pragma region // Structure manipulation functions.
+#pragma endregion
+
+#pragma region // Structure-array manipulation functions.
 #pragma endregion
 
 int main() {
