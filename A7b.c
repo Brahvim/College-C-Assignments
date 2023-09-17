d7ad293016fd4409d0d7d829612b72d2ba2a1cb9
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-09-17 14:32:12 +0530

    Is this the first commit? I'm literally DONE!

---
+++ A7b.c
@@ -0,0 +1,33 @@
+// Author: Brahvim Bhaktvatsal
+
+#pragma region // Header declarations.
+#include "include/IoUtilsByBrahvim.h"
+
+typedef unsigned long long ull;
+
+// Functions:
+bool is_prime(const ull number);
+
+DECLARE_GENERIC_INPUT_FUNCTIONS(ull);
+#pragma endregion
+
+DEFINE_GENERIC_INPUT_FUNCTIONS(ull, "%llu");
+
+int main() {
+	puts("Welcome to the naive-algorithm, prime number generation program!");
+	const ull count = ensure_user_inputs_ull("number of prime numbers to find!");
+
+	for (ull i = 0, found = 0; found != count; i++)
+		if (is_prime(i))
+			printf("Prime `%llu`: `%llu`.\n", ++found, i);
+}
+
+bool is_prime(const ull p_number) {
+	bool flag = true;
+	const ull check_till = p_number / 2 + 1;
+
+	for (ull i = 2; i < check_till; i++)
+		flag = p_number % i != 0;
+
+	return flag;
+}
