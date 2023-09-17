168dd7b80ea1223b7ecd9f1f92418ba9e7590542
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-09-17 21:08:44 +0530

    This time, we'll play safe with the branches.

---
+++ A8.c
@@ -0,0 +1,15 @@
+// Author: Brahvim Bhaktvatsal
+
+#pragma region // Header declarations.
+#include "include/IoUtilsByBrahvim.h"
+
+struct student;
+struct result;
+struct marks;
+#pragma endregion
+
+#pragma region // Struct defintions.
+#pragma endregion
+
+int main() {
+}
