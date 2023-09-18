a07bb0d1638385b6f11573e02234cffb1e0f8687
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-09-18 17:43:01 +0530

    ...Wait,

--- A8.c
+++ A8.c
@@ -6,10 +6,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MALLOC_TYPE(type) (type *)malloc(sizeof(type))
+
+#define CHECKED_MALLOC(ptr_name, type, on_malloc_fail)          \
+type *ptr_name = MALLOC_TYPE(type);                             \
+    if (ptr_name == NULL) on_malloc_fail // Macro functions generally receive a semi-colon anyway.
+
+#define DECLARE_MALLOCATOR(type) bool create_##type(const type **storage_addr)
+
+#define DECLARE_STRUCT_MALLOCATOR(struct_name)                                  \
+bool create_ ## struct_name ## _struct(const struct struct_name **storage_addr)
+
+#define DEFINE_MALLOCATOR(type, custom_config)                      \
+DECLARE_MALLOCATOR(type) {                                          \
+    CHECKED_MALLOC(to_fill, type, return false);                    \
+    *storage_addr = to_fill;                                        \
+    custom_config;                                                  \
+    return true;                                                    \
+}
+
+#define DEFINE_STRUCT_MALLOCATOR(type, custom_config)                       \
+DECLARE_STRUCT_MALLOCATOR(type) {                                           \
+    CHECKED_MALLOC(to_fill, type, return false);                            \
+    custom_config;                                                          \
+    return true;                                                            \
+}
+
+
 // Structures (following the convention for the Linux kernel - without `typedef`s):
 struct student;
 struct result;
 #pragma endregion
+
+DECLARE_STRUCT_MALLOCATOR(student);
 
 #pragma region // Structure definitions.
 struct student {
@@ -26,37 +55,39 @@ struct result {
 
 #pragma region // Structure de/-allocation function definitions.
 #pragma region // Creation,
-bool create_student_struct(struct student *p_to_fill) {
-    // Allocate one!:
-    struct student *to_ret = (struct student *)malloc(sizeof(struct student));
+// bool create_student_struct(const struct student **p_storage_addr) {
+//     // Allocate one!:
+//     CHECKED_MALLOC(to_fill, struct student, return false);
 
-    if (to_ret == NULL)
+//     // Assign to caller's pointer:
+//     *p_storage_addr = to_fill;
+
+DEFINE_MALLOCATOR(int, printf("LOL"));
+
+DEFINE_STRUCT_MALLOCATOR(student, {
+    to_fill->regno = 0L;
+    strcpy(to_fill->name, "");
+    });
+
+//     // ...And now, return it:
+//     return true;
+// }
+
+bool create_result_struct(const struct result **p_to_fill) {
+    // Allocate one!:
+    struct result *to_fill = (struct result *)malloc(sizeof(struct result));
+
+    if (to_fill == NULL)
         return false;
 
-    p_to_fill = to_ret;
+    *p_to_fill = to_fill;
 
-    to_ret->regno = 0L;
-    strcpy(to_ret->name, "");
-
-    // ...And now, return it:
-    return true;
-}
-
-bool create_result_struct(struct result *p_to_fill) {
-    // Allocate one!:
-    struct result *to_ret = (struct result *)malloc(sizeof(struct result));
-
-    if (to_ret == NULL)
-        return false;
-
-    p_to_fill = to_ret;
-
-    to_ret->total = 0;
+    to_fill->total = 0;
     // to_ret->student = p_student_ptr;
 
-    to_ret->math = 0;
-    to_ret->science = 0;
-    to_ret->language = 0;
+    to_fill->math = 0;
+    to_fill->science = 0;
+    to_fill->language = 0;
 
     // ...And now, return it:
     return true;
@@ -64,17 +95,30 @@ bool create_result_struct(struct result *p_to_fill) {
 #pragma endregion
 
 #pragma region // Deletion...
-struct student *delete_student_struct(struct student *p_student_ptr) {
-    if (p_student_ptr != NULL)
-        // ...Before it's too LATE:
-        free(p_student_ptr);
+bool delete_student_struct(struct student **p_student_ptr) {
+    struct student *actual_object = *p_student_ptr;
+
+    if (p_student_ptr == NULL || actual_object == NULL)
+        return false;
+
+    // ...Before it's too LATE:
+    free(actual_object);
+    *p_student_ptr = NULL;
+    return true;
 }
 
-struct result *delete_result_struct(struct result *p_result_ptr) {
-    if (p_result_ptr != NULL)
-        // ...Before it's too LATE:
-        free(p_result_ptr);
+bool delete_result_struct(struct result **p_result_ptr) {
+    struct result *actual_object = *p_result_ptr;
+
+    if (p_result_ptr == NULL || actual_object == NULL)
+        return false;
+
+    // ...Before it's too LATE:
+    free(actual_object);
+    *p_result_ptr = NULL;
+    return true;
 }
+
 #pragma endregion
 #pragma endregion
 
