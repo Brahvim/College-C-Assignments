94612b016149512dfc71ab9ce377f732681564bc
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-09-19 11:11:37 +0530

    NO MORE Macro craziness! Commiting from college.

--- A8.c
+++ A8.c
@@ -6,69 +6,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#pragma region // Macros!
-/** Allocates using `malloc()` given a type. */
-#define MALLOC_TYPE(type) (type *)malloc(sizeof(type))
-
-/** Allocates using `malloc()`, given a name for the allocated pointer, the type of the data to be allocated,
- * and optionally, a code block (or a single line) to run when it fails.
- */
-#define CHECKED_MALLOC(ptr_name, type, on_malloc_fail)          \
-    type *ptr_name = MALLOC_TYPE(type);                         \
-    if (ptr_name == NULL) on_malloc_fail
-; // <-- Placed to fix an issue caused by the VSCode formatter (it places an empty space before the next comment!).
-
-/** Declares a function to use `malloc()` to allocate something of the given type. */
-#define DECLARE_MALLOCATOR(type) bool create_##type(const type **storage_addr)
-
-/** Declares a function to use `malloc()` to allocate a `struct` given only its name.
- * Use `DECLARE_MALLOCATOR` instead if your structs were `typedef`d.
- */
-#define DECLARE_STRUCT_MALLOCATOR(struct_name)                                  \
-bool create_ ## struct_name ## _struct(const struct struct_name **storage_addr)
-; // <-- Placed to fix an issue caused by the VSCode formatter (it places an empty space before the next comment!).
-
-#define MALLOCATOR_DEFINITION(type, ptr_name, custom_config) {                  \
-    /* Allocate one!: */                                                        \
-    CHECKED_MALLOC(ptr_name, type, return false);                               \
-    /* Assign to caller's pointer!: */                                          \
-    *storage_addr = ptr_name;                                                   \
-    custom_config;                                                              \
-    return true;                                                                \
-}
-
-/** Defines a function to use `malloc()` to allocate for a given:
- * 1. Type,
- * 2. Name of the pointer holding `malloc()`'s return value.
- * 3. A code block / single line to make further changes to said pointer.
- *
- * The function thus defined takes in a double-pointer of the given type.
- * (So assigning it to the received double-pointer, as well as further changes, are possible.)
- *
- * It returns `true` or `false` from `stdbool.h` to indicate the allocation's status.
- *
- * Example:
- * The following defines `create_int(int** storage_addr)`, a which allocates an integer and sets it to `0`:
- * `DEFINE_MALLOCATOR(int, ptr, { *ptr = 0; })`.
- *
- * Here's how the function is to be used:
- * `int *a; create_int(&a);` - this allocates an `int` on the heap, and sets it to `0`. It can be accessed using `a`.
- */
-#define DEFINE_MALLOCATOR(type, ptr_name, custom_config)    \
-DECLARE_MALLOCATOR(type)                                    \
-MALLOCATOR_DEFINITION(type, ptr_name, custom_config)
-
-#define DEFINE_STRUCT_MALLOCATOR(type, ptr_name, custom_config)                             \
-DECLARE_STRUCT_MALLOCATOR(type) MALLOCATOR_DEFINITION(struct type, ptr_name, custom_config)
-; // <-- Placed to fix an issue caused by the VSCode formatter (it places an empty space before the next comment!).
-#pragma endregion
-
-#pragma region // Function declarations.
-// For structure de/-allocation:
-DECLARE_STRUCT_MALLOCATOR(student);
-DECLARE_STRUCT_MALLOCATOR(result);
-#pragma endregion
-
 // Structures (following the convention for the Linux kernel - without `typedef`s):
 struct student;
 struct result;
@@ -86,57 +23,6 @@ struct result {
     float language, math, science;
 } /* __attribute__((packed)) */;
 #pragma endregion
-
-#pragma region // Structure de/-allocation function definitions.
-#pragma region // Creation,
-DEFINE_STRUCT_MALLOCATOR(student, ptr, {
-    ptr->regno = 0L;
-    strcpy(ptr->name, "");
-});
-
-DEFINE_STRUCT_MALLOCATOR(result, ptr, {
-    ptr->total = 0;
-    ptr->student = NULL;
-
-    ptr->math = 0;
-    ptr->science = 0;
-    ptr->language = 0;
-});
-#pragma endregion
-
-#pragma region // Deletion...
-bool delete_student_struct(struct student **p_student_ptr) {
-    struct student *actual_object = *p_student_ptr;
-
-    if (p_student_ptr == NULL || actual_object == NULL)
-        return false;
-
-    // ...Before it's too LATE:
-    free(actual_object);
-    *p_student_ptr = NULL;
-    return true;
-}
-
-bool delete_result_struct(struct result **p_result_ptr) {
-    struct result *actual_object = *p_result_ptr;
-
-    if (p_result_ptr == NULL || actual_object == NULL)
-        return false;
-
-    // ...Before it's too LATE:
-    free(actual_object);
-    *p_result_ptr = NULL;
-    return true;
-}
-#pragma endregion
-#pragma endregion
-
-#pragma region // Structure manipulation function definitions.
-#pragma endregion
-
-#pragma region // Structure-array manipulation functions.
-#pragma endregion
-
 
 int main() {
 }
