0a6638fed847180e95ba8bea3e1a2a2e70334fb4
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-09-21 22:57:28 +0530

    It works! May none EVER need to hand-write code...

--- A8.c
+++ A8.c
@@ -2,10 +2,7 @@
 
 #pragma region // Header declarations.
 #include "include/IoUtilsByBrahvim.h"
-#include <math.h>
 #include <stdlib.h>
-#include <string.h>
-#include <limits.h>
 
 typedef unsigned long long ull;
 
@@ -15,13 +12,12 @@ typedef unsigned long long ull;
 DECLARE_GENERIC_INPUT_FUNCTIONS(ull);
 DECLARE_GENERIC_INPUT_FUNCTIONS(float);
 
-// "Keep it simple, stupid!":
-// PS These declarations follow the Linux kernel's naming convention (no `typedef`).
+// These declarations follow the Linux kernel's naming convention (no `typedef`).
 struct student;
 #pragma endregion
 
-DEFINE_GENERIC_INPUT_FUNCTIONS(ull, "%llu");// Structures (following the convention for the Linux kernel - without `typedef`s):struct student;
-DEFINE_GENERIC_INPUT_FUNCTIONS(float, "%f");// Structures (following the convention for the Linux kernel - without `typedef`s):struct student;
+DEFINE_GENERIC_INPUT_FUNCTIONS(ull, "%llu");
+DEFINE_GENERIC_INPUT_FUNCTIONS(float, "%f");
 
 struct student {
     ull regno;
@@ -34,6 +30,7 @@ int main() {
     puts("Welcome to the marksheet-generation program!");
     ull num_students = ensure_user_inputs_ull("number of students");
 
+    // All ways out exit the program. Thus I never `free()` this:
     struct student *students_array = (struct student *)calloc(num_students, sizeof(struct student));
 
     if (students_array == NULL) {
@@ -41,11 +38,13 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    for (ull i = 1; i <= num_students; i++) {
-        struct student *current_student = students_array + i * sizeof(struct student);
+    for (ull i = 0; i < num_students; i++) {
+        struct student *current_student = &students_array[i];
 
-        puts("For student `%llu`:");
+        printf("For student `%llu`:\n", i + 1);
         printf("Please enter the name (length below `%llu`, in ASCII): ", MAX_NAME_LEN);
+        for (char c; !((c = getchar()) == '\n' || c == EOF);)
+            ;
 
         for (ull name_len = 0; name_len < MAX_NAME_LEN; name_len++) {
             char c = getchar();
@@ -56,7 +55,6 @@ int main() {
             current_student->name[name_len] = c;
         }
 
-        clear_stdin();
         current_student->regno = ensure_user_inputs_ull("registration number");
 
         printf("Please enter marks for all `%llu` subjects:\n", NUM_SUBJECTS);
@@ -74,9 +72,5 @@ int main() {
         struct student current_student = students_array[i];
         printf("Total for %s: `%f`.\n", current_student.name, current_student.total);
     }
-
-
-    // Only one free at the end. Nowhere else.
-    free(students_array);
 
 }
