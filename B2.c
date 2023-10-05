afd70452d85b0222d31f11d2df1d79731494fb1c
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-10-05 15:22:31 +0530

    Weak overflow check...

--- B2.c
+++ B2.c
@@ -1,18 +1,32 @@
+#include <limits.h>
 #include "./include/IoUtilsByBrahvim.h"
+
+#pragma region // Header declarations.
 typedef unsigned long long ull;
 
 DECLARE_GENERIC_INPUT_FUNCTIONS(ull);
+#pragma endregion
+
 DEFINE_GENERIC_INPUT_FUNCTIONS(ull, "%lld");
 
 int main() {
 	const ull uin = ensure_user_inputs_ull(
 		"number of elements of the fibonacci series you want printed");
 
-	ull x = 0, y = 1, z;
+	ull x = 0, y = 1, z, last = 0;
 
 	for (ull i = 0; i < uin; i++) {
+		// Check for overflow:
+		if (x < last) {
+			puts("64-bit limit hit! Stopping...");
+			break;
+		}
+
 		// Print it:
 		printf("`%lld`\n", x);
+
+		// Store the last value:
+		last = x;
 
 		// Add to the current number from the sequence:
 		z = x + y;
