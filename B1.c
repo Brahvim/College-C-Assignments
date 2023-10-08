84ced5d78a349e6cc37d0797089c7ba9c82a0620
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-10-08 11:02:41 +0530

    "Improvements"!

--- B1.c
+++ B1.c
@@ -20,30 +20,34 @@ DEFINE_GENERIC_INPUT_FUNCTIONS(ull, "%llu");
 int main() {
   puts("Welcome to the digit manipulation program!\n");
 
-  ull input_number = ensure_user_inputs_ull("digit you want analyzed");
+  // What if we want to use this number later?:
+  const ull input_number = ensure_user_inputs_ull("digit you want analyzed");
+
+  ull num_to_analyze = input_number; // Will modify this in calculations!
 
   uchar num_digits = 1;  // No 64-bit `ull` can have `255` digits or more, so we use a `uchar` for this.
-  // We set this to `1` for now, then decrease it after the loop in which we show reversed digits, since it needs to
-  // have `1` as the first index.
+  // We set this to `1` for now, then decrease it after the loop in which we show reversed digits, since
+  // it needs to have `1` as the first index.
 
-  // The sum of the digits of `1,84,46,74,40,73,70,95,51,615`, the largest `ull` AKA
-  //  `64`-bit number, is `87`. Therefore, I use a `uchar` for storing the sum.
-  uchar digits_sum = 0;
+  uchar digits_sum = 0; // I use a `uchar` for storing the sum since the sum of the digits of
+  // `1,84,46,74,40,73,70,95,51,615`, the largest `ull` AKA `64`-bit number, is `87`.
 
-  puts("Here are all digits in the number, in reverse order:");
+  printf("In reverse, that'd be: `");
 
-  for (; input_number != 0; num_digits++) {
-    uchar digit = input_number % 10;  // Extract a digit.
-    digits_sum += digit;              // Add to the sum of digits.
-    input_number /= 10;               // Modify the original number!
-
-    printf("Digit `%d`: `%d`.\n", num_digits, digit);  // ...Show it off!
+  for (; num_to_analyze; num_digits++) {
+    const uchar digit = num_to_analyze % 10;  // Extract a digit.
+    digits_sum += digit;                      // Add to the sum of digits.
+    num_to_analyze /= 10;                     // Modify the original number!
+    printf("%d", digit);                      // Show the digit!
   }
 
-  num_digits -= 1;  // When showing each digit in the loop, we needed to count from `1`. Now we don't!
+  puts("`.");
 
-  printf("Thus, the total number of digits is: `%d`.\n", num_digits);
+  // When showing each digit in the loop,
+  // we needed to count from `1`.
+  // Now we don't!:
+  num_digits--;
+
+  printf("The number of digits is: `%d`.\n", num_digits);
   printf("Their sum, is: `%d`.\n", digits_sum);
-
-  return 0;
 }
