2bd7d196dc8f162d0085913af8c5a3f190d17158
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-10-08 10:43:45 +0530

    Literally done!

--- B2b.c
+++ B2b.c
@@ -1,5 +1,23 @@
 #include "include/IoUtilsByBrahvim.h"
 
+#pragma region // Header declarations.
+
+typedef unsigned long long ull;
+
+DECLARE_GENERIC_INPUT_FUNCTIONS(ull);
+DEFINE_GENERIC_INPUT_FUNCTIONS(ull, "%llu");
+
+#pragma endregion
+
 int main() {
-    
+
+    ull year = ensure_user_inputs_ull("year you want checked");
+
+    puts(
+        year % 4 == 0
+        && year % 100 == 0
+        && year % 400 == 0
+        ? "This year is a leap year!"
+        : "This year is not a leap year...");
+
 }
