9377a41affec91945207a56758e23c479b068eaa
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-10-29 10:37:05 +0530

    Formatting correction.

--- B3.c
+++ B3.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
+
 int main(const int p_count, char const **p_args) {
     if (p_count < 2)
-        puts("This program prints arguments provided to it in reverse. Please consider providing some.");
+        puts("This program prints arguments provided to it in reverse.\n"
+            "Please consider providing some.");
     else
         for (int i = p_count - 1; i > 0; i--)
             puts(p_args[i]);
