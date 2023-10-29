90fcea2fef03c7c90c409b639654a73ce6f18406
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-10-29 10:44:47 +0530

    Added some docs!

--- B3.c
+++ B3.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main(const int p_count, char const **p_args) {
-    if (p_count < 2)
+    // `p_args` would be in the format, `B3.out [ARGS]`
+    // - the name of the program is still in there!
+
+    if (p_count < 2) // Thus, we always need two or more elements in `p_args`.
         puts("This program prints arguments provided to it in reverse.\n"
             "Please consider providing some.");
     else
