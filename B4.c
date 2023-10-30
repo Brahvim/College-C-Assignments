a4c1cf02895b1a4f3ae9edfc2f0c0611c66a5c80
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-10-30 23:30:07 +0530

    `stackoverflow/a/52984954/` - nice place to learn.

--- B4.c
+++ B4.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <ctype.h>
-
-/*
-    { BasedOnStyle: Google, IndentWidth: 4, ContinuationIndentWidth: 4, ColumnLimit: 120, AlignAfterOpenBracket:
-    AlwaysBreak, AlignConsecutiveAssignments: false, AlignConsecutiveDeclarations: true,
-    AllowAllParametersOfDeclarationOnNextLine: false, AllowShortBlocksOnASingleLine: false,
-    AllowShortCaseLabelsOnASingleLine: false, AllowShortFunctionsOnASingleLine: None,
-    AllowShortIfStatementsOnASingleLine: Never, AllowShortLoopsOnASingleLine: false, IndentCaseLabels: true,
-    IndentWrappedFunctionNames: false, KeepEmptyLinesAtTheStartOfBlocks: true, SpaceBeforeParens: ControlStatements,
-    TabWidth: 4, UseTab: Always, BreakBeforeTernaryOperators: true, BreakAfterTernaryColon: false,
-    BinPackArguments: false, AlignAfterOpenBracket: DontAlign, }
-*/
 
 #pragma region // Header declarations.
 typedef char strch_t; // This is to help switch encodings later if needed.
@@ -20,95 +10,99 @@ typedef char strch_t; // This is to help switch encodings later if needed.
 #pragma endregion
 
 int main() {
-    size_t num_names = 0;
-    size_t space_for_names = 5;
-    strch_t **names = calloc(space_for_names, sizeof(strch_t *));
+	size_t num_names = 0;
+	size_t space_for_names = 5;
+	strch_t **names = calloc(space_for_names, sizeof(strch_t *));
 
-    if (names == NULL) {
-        perror("Startup error. Cannot continue...");
-        return EXIT_FAILURE;
-    }
+	if (names == NULL) {
+		perror("Startup error. Cannot continue...");
+		return EXIT_FAILURE;
+	}
 
-    puts("Welcome to the string-sorting program!");
-    puts("Enter some strings (across lines) that you want sorted.");
+	puts("Welcome to the string-sorting program!");
+	puts("Enter some strings (across lines) that you want sorted.");
 
-    // This information is retained across loops 
-    // to make sure large strings can be entered:
-    size_t new_name_len = 0;
-    strch_t *new_name = { 0 };
+	// This information is retained across loops
+	// to make sure large strings can be entered:
+	size_t new_name_len = 0;
+	strch_t *new_name = { 0 };
 
-    for (strch_t buf[MAX_SIZE_FOR_READ_STRINGS] = { 0 };
-        // `fgets()` returns `NULL` when it reads `EOF` from the given `FILE` handle.
-        fgets(buf, MAX_SIZE_FOR_READ_STRINGS, stdin) != NULL;) {
+	// You know what? I'll just learn from: [ https://stackoverflow.com/questions/52984551/using-fgets-with-realloc ].
 
-        // Add more space to the array if needed:
-        if (num_names >= space_for_names) {
-            // ...^^^ really just an equality check. 
-            // Any overflow would cause a crash.
+	for (strch_t buf[MAX_SIZE_FOR_READ_STRINGS] = { 0 };
+		// `fgets()` returns `NULL` when it reads `EOF` from the given `FILE` handle.
+		fgets(buf, MAX_SIZE_FOR_READ_STRINGS, stdin) != NULL;) {
 
-            space_for_names += 5; // Allocate a bit more in advance.
-            // It's for just pointers anyway.
+		// Add more space to the array if needed:
+		if (num_names >= space_for_names) {
+			// ...^^^ really just an equality check.
+			// Any overflow would cause a crash.
 
-            strch_t **names_array_reallocated = realloc(names, space_for_names);
+			space_for_names += 5; // Allocate a bit more in advance.
+			// It's for just pointers anyway.
 
-            if (names_array_reallocated == NULL) {
-                perror("Allocating more memory for more names failed!");
-                break;
-            } else
-                names = names_array_reallocated;
-        }
+			strch_t **names_array_reallocated = realloc(names, space_for_names * sizeof(strch_t *));
 
-        const size_t len = strlen(buf), len_minus_1 = len - 1;
+			if (names_array_reallocated == NULL) {
+				perror("Allocating more memory for more names failed!");
+				break;
+			} else
+				names = names_array_reallocated;
+		}
 
-        // We want more memory at this point:
-        new_name_len += len;
-        new_name = realloc(new_name, new_name_len);
+		const size_t len = strlen(buf), len_minus_1 = len - 1;
 
-        // Copy the part of the string we read:
-        strncpy(new_name + new_name_len, buf, len + 1);
+		// We want more memory at this point:
+		new_name = realloc(new_name, new_name_len + len + 1);
 
-        // This replaces the `\n` with a `\0`:
-        if (len > 0 && buf[len_minus_1] == '\n') {
-            new_name[len_minus_1] = '\0'; // If we can, we end the string early.
+		// Copy the part of the string we read:
+		strncpy(new_name + new_name_len, buf, len + 1);
 
-            // No need to clear `buf` like via this line here:
-            // memset(buf, 0, MAX_SIZE_FOR_READ_STRINGS);
-            // If the new string's longer that the contents in `buf`, good.
-            // The new string written into `buf` will have a `\0` before the older data anyway.
+		// This replaces the `\n` with a `\0`:
+		if (len > 0 && buf[len_minus_1] == '\n') {
+			new_name[len_minus_1] = '\0'; // If we can, we end the string early.
 
-            // A new name will be placed there, so we reset the length here:
-            new_name_len = 0;
+			// No need to clear `buf` like via this line here:
+			// memset(buf, 0, MAX_SIZE_FOR_READ_STRINGS);
+			// If the new string's longer that the contents in `buf`, good.
+			// The new string written into `buf` will have a `\0` before the older data anyway.
 
-            // Increment and assign!:
-            names[num_names++] = new_name;
-        }
+			// Increment and assign!:
+			names[num_names++] = new_name;
 
-    }
+			// A new name will be placed there, so we reset the length here:
+			new_name = NULL;
+			new_name_len = 0;
+		}
 
-    puts("\nSorting using bubble sort...\n");
+	}
 
-    // Bubble-sorting the array of names:
-    for (size_t i = 0; i <= num_names; i++)
-        for (size_t j = 0; j <= num_names - i; j++) {
-            // .............................. ^^^ the `- i` is an optimization.
-            if (names[j] == NULL) {
-                perror("One of the strings you gave was `NULL`! Cannot sort this...");
-                continue; // exit(EXIT_FAILURE);
-            } else if (strcmp(names[j], names[j + 1]) > 0) {
-                const char *temp = names[j];
-                names[j] = names[j + 1];
-                names[j + 1] = temp;
-            }
-        }
+	puts("\nSorting using bubble sort...\n");
 
-    // De-allocating space allocate for each name-string:
-    for (size_t i = 0; i < num_names; i++) {
-        char *name = names[i];
-        puts(name);
-        free(name);
-    }
+	// FIXME Bubble-sorting the array of names:
+	// for (size_t i = 0; i <= num_names; i++)
+	//     for (size_t j = 0; j <= num_names - i; j++) {
+	//         // .............................. ^^^ the `- i` is an optimization.
+	//         if (names[j] == NULL) {
+	//             perror("One of the strings you gave was `NULL`! Cannot sort this...");
+	//             continue; // exit(EXIT_FAILURE);
+	//         } else if (strcmp(names[j], names[j + 1]) > 0) {
+	//             const char *temp = names[j];
+	//             names[j] = names[j + 1];
+	//             names[j + 1] = temp;
+	//         }
+	//     }
 
-    // De-allocating space used by the pointers:
-    puts("Done. Bye now!");
-    free(names);
+	// De-allocating space allocate for each name-string:
+	for (size_t i = 0; i < num_names; i++) {
+		char *name = names[i];
+		puts(name);
+		free(name);
+	}
+
+	// De-allocating space used by the pointers:
+	puts("Done. Bye now!");
+	free(names);
+
+	return EXIT_SUCCESS;
 }
