87daf49c2b58a0f5b1606ca241ce9c7a142a2188
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-12-19 17:46:03 +0530

    Dunno, it works!...

--- B4.c
+++ B4.c
@@ -12,15 +12,31 @@ DEFINE_GENERIC_INPUT_FUNCTIONS(size_t, "%llu");
 int main() {
     puts("Welcome to the string sorting program!");
 
-    const size_t num_str_estimate = ensure_user_inputs_size_t("Any idea of how many strings we will need?");
-    char **strings_array = calloc(num_str_estimate, sizeof(char*));
+    size_t strings_array_available_space = ensure_user_inputs_size_t("Any idea of how many strings we will need?");
+    char **strings_array = calloc(strings_array_available_space, sizeof(char*));
+    size_t strings_array_size = 0;
 
     if (!strings_array)
         perror("Too many strings for your computer to handle, sorry.");
 
     while (getchar() != EOF) {
-        
+        if (strings_array_size == strings_array_available_space) {
+            strings_array_available_space += 5;
+
+            char **new_strings_array = realloc(strings_array, strings_array_available_space);
+            if (!new_strings_array) {
+                perror("Out of memory we are! Program exiting...");
+                goto cleanup;
+            }
+
+            
+
+        }
+
+        strings_array[strings_array_size] = read_line(5);
+        strings_array_size++;
     }
 
+cleanup:
     free(strings_array);
 }
